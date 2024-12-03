72b99487d0e4662c05f7ebe221ac08f217230b25
majikarikeruo <castero1219@gmail.com>
2024-12-03 18:46:20 +0900

    [add]入力フォームのStyleを追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Entry from "@/pages/Entry";
 import Result from "@/pages/Result";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "@/style.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/entry" element={<Entry />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-    </BrowserRouter>
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/entry" element={<Entry />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   );
 }
 
--- src/pages/Entry.tsx
+++ src/pages/Entry.tsx
@@ -1,3 +1,52 @@
+import { Tabs, Title, Text, Container, Flex, FileInput, Input, Stack, Button, Group } from "@mantine/core";
+
 export default function Entry() {
-  return <div>Entry</div>;
+  return (
+    <Stack py={40}>
+      <Flex direction="column" align="center" justify="center" gap="md" mb={16}>
+        <Text align="center">オーディエンスの反応を予測するプレゼン作成支援ツール🎨</Text>
+        <Title align="center">Pitch Cart</Title>
+        <Text align="center">ステークホルダーに あなたの思いが的確に伝わる</Text>
+      </Flex>
+
+      <Container size="sm" style={{ backgroundColor: "white", borderRadius: 16 }} px={16} pt={8} py={24}>
+        <Tabs defaultValue="pitch">
+          <Tabs.List my={16}>
+            <Tabs.Tab value="pitch">プレゼンチェックモード</Tabs.Tab>
+            <Tabs.Tab value="rehearsal">リハーサルモード（作成中）</Tabs.Tab>
+            <Tabs.Tab value="simulation">シミュレーションモード（作成中）</Tabs.Tab>
+          </Tabs.List>
+
+          <Tabs.Panel value="pitch">
+            <Stack my={16}>
+              <Input.Wrapper label="オーディエンス相手は誰ですか？">
+                <Input placeholder="例）Tech系VC" />
+              </Input.Wrapper>
+              <Input.Wrapper label="プレゼンの目的は何ですか？">
+                <Input placeholder="例）シリーズA資金調達" />
+              </Input.Wrapper>
+
+              <FileInput
+                label="プレゼン資料をアップロードしてください"
+                placeholder="ここにスライドの資料をドラッグするか、クリックしてファイルを選択してください(pptx。容量●MB。)"
+              />
+              <FileInput
+                label="プレゼン原稿をアップロードしてください"
+                placeholder="ここにスライドの資料をドラッグするか、クリックしてファイルを選択してください(pptx。容量●MB。)"
+              />
+            </Stack>
+            <Group justify="center">
+              <Button>分析を開始する</Button>
+            </Group>
+          </Tabs.Panel>
+          <Tabs.Panel value="rehearsal">
+            <Text align="center">リハーサルモード</Text>
+          </Tabs.Panel>
+          <Tabs.Panel value="simulation">
+            <Text align="center">シミュレーションモード</Text>
+          </Tabs.Panel>
+        </Tabs>
+      </Container>
+    </Stack>
+  );
 }
